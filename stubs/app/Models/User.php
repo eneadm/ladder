b694545aa08eead7a7106adc557bb6b05a32ab8b
Enea Dhiamandi <enea.dhiamandi@klimahelden.eu>
2023-08-15 12:09:32 +0200

    Small fix

--- stubs/app/Models/User.php
+++ stubs/app/Models/User.php
@@ -14,44 +14,19 @@ class User extends Authenticatable
     use Notifiable;
     use HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
-    protected $appends = [
-        'profile_photo_url',
+        'password' => 'hashed',
     ];
 }
